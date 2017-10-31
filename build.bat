38d778731c85a2cf997d7fd99f3fd36c6e26b380
Aiden <kkituyi@yahoo.com>
2017-10-31 21:38:25 +0300

    sdk manager issue

--- build.bat
+++ build.bat
@@ -11,7 +11,7 @@ set ANDROID_HOME=C:\android\android-sdk-windows
 set PATH=%PATH%;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools
 dir  %ANDROID_HOME%
 
-echo y | android --no-ui --all --filter  android-24,platform-tools,tools,build-tools-24.0.3
+echo y | android --silent update sdk --no-ui  --filter android-24,platform-tools,tools,build-tools-24.0.3
 echo "Running gradle"
 
 ./gradlew.bat
