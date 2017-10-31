a5367fa185ec4ee9cb7a168cf30092dd0b3a62b8
Aiden <kkituyi@yahoo.com>
2017-10-31 19:56:52 +0300

    Works

--- build.bat
+++ build.bat
@@ -3,8 +3,11 @@ set ANDROID_ZIP=http://dl.google.com/android/android-sdk_r22.0.5-linux.tgz
 if not exist downloads mkdir downloads
 if not exist %ANDROID_HOME% mkdir %ANDROID_HOME%
 call :downloadfile %ANDROID_ZIP% downloads/SDK.zip
-7z x downloads\SDK.zip -o%ANDROID_HOME% > nul || exit 1
+echo "Finished downloading"
+7z x downloads\SDK.zip -o%ANDROID_HOME% 
+echo "Finished extracting"
 (while sleep 3; do echo "y"; done) |  %ANDROID_HOME%tools\bin>android --silent update sdk --no-ui  --filter android-24,platform-tools,tools,build-tools-24.0.3
+echo "Running gradle"
 ./gradlew.bat
 ./gradlew.bat assembleDebug
 
